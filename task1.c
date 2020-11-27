9253dd3bb101f2cdf7ca06cd10040b6d62dc4e14
SuperPro 2009 <deremenko2009@gmail.com>
2020-11-27 22:22:36 +0300

    new

---
+++ task1.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int b = 0
+    for(int i = 0;i++;b++){
+         if (i==2){
+            b = b+(i-i*2);
+        };
+         else if (i==4){
+            b = b+(i-i*2);
+        };
+         else if (i==6){
+            b = b+(i-i*2);
+        };
+         else if (i==8){
+            b = b+(i-i*2);
+        };
+         else if (i==10){
+            b = b+(i-i*2);
+        };
+         else if (i==12){
+            b = b+(i-i*2);
+        };
+         else if (i==14){
+            b = b+(i-i*2);
+        };
+         else{
+             b=b+i;
+             
+        };
+         
+    }
+    printf(b);
+};
---
+++ task2.c
@@ -0,0 +1,108 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int
+main ()
+{
+  int t = 0 for (int t = /*P7P=P0QP5P=P8P5 t */ ; i < 11 i++)
+    {
+
+      if (i == 1)
+	{
+	  if (t < -24)
+	    {
+	      printf ("-5");
+	    };
+	};
+      else
+    if (i == 2)
+      {
+	else
+      if (-24 <= t < -10)
+	{
+	  printf ("-4");
+	};
+      };
+      else
+    if (i == 3)
+      {
+	else
+      if (-10 <= t <= 0)
+	{
+	  printf ("-3");
+	};
+      };
+      else
+    if (i == 4)
+      {
+	else
+      if (0 < t < 12)
+	{
+	  printf ("-2");
+	};
+      };
+      else
+    if (i == 5)
+      {
+	else
+      if (12 <= t < 20)
+	{
+	  printf ("-1");
+	};
+      };
+      else
+    if (i == 6)
+      {
+	else
+      if (20 <= t < 24)
+	{
+	  printf ("0");
+	};
+      };
+      else
+    if (i == 7)
+      {
+	else
+      if (24 <= t < 28)
+	{
+	  printf ("1");
+	};
+      };
+      else
+    if (i == 8)
+      {
+	else
+      if (28 <= t < 34)
+	{
+	  printf ("2");
+	};
+      };
+      else
+    if (i == 9)
+      {
+	else
+      if (34 <= t < 40)
+	{
+	  printf ("3");
+	};
+      };
+      else
+    if (i == 10)
+      {
+	else
+      if (t > 40)
+	{
+	  printf ("4");
+	};
+      };
+
+      return 0;
+    };
+};
